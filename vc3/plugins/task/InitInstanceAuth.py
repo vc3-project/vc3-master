6fed2e57716010f295ca5d3f909143bf6c21c130
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-04-24 17:13:55 -0400

    Update.

--- vc3/plugins/task/InitInstanceAuth.py
+++ vc3/plugins/task/InitInstanceAuth.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 # 
-#  This Task plugin searches for new user vcluster Requests
-#
-#  - poll for requests in state 'new'
-#  --  determine need for core
-#  if CORE:
-#     -- generate factory config  
-#     -- launch core instance
-#     -- confirm core launched 
-#     -- store request in state 'initialized'
 #
 from vc3.task import VC3Task
 
 class InitInstanceAuth(VC3Task):
     '''
-    Plugin to transition Requests from 'new' to 'initialized' state.
+    Plugin to do first-time overall VC3 instance setup. 
+    Confirm that CA exists, create host cert for this server. 
      
     '''
     
