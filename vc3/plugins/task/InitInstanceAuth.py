e2f46efd93bc79ca9e9f20fa794217e8b548826c
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-04-28 13:11:53 -0400

    Update.

--- vc3/plugins/task/InitInstanceAuth.py
+++ vc3/plugins/task/InitInstanceAuth.py
@@ -23,26 +23,28 @@ class InitInstanceAuth(VC3Task):
         '''
         self.log.debug("Running task %s" % self.section)
         self.log.debug("Getting 'vc3' doc.")
-        doc = self.parent.parent.infoclient.getdocument('vc3')
-        
         try:
-            ds = json.loads(doc)
+            doc = self.parent.parent.infoclient.getdocument('vc3')
+            self.log.debug('Doc is %s' % doc)
         except Exception as e:
-            self.log.error("No doc at all.")
-            raise e
+            self.log.error("Exception: %s" % e)
 
-        try:
-            requests = ds['vc3']
-            self.log.debug("vc3 section already in doc. Doing nothing.")
-        except KeyError:
-            # no vc3 section
-            self.log.debug("No vc3 section in doc.")
-            ccstr = self.parent.parent.ssca.getcertchain()
-            eccstr = self.parent.parent.infoclient.encode(ccstr)
-            
-            ds['vc3'] = { "ca-chain" : eccstr, "encoding" : "base64" }
-            jd = json.dumps(ds)
-            self.parent.parent.infoclient.storedocument('vc3',jd)
-            
+        if doc is not None:            
+            try:
+                ds = json.loads(doc)
+                try:
+                    requests = ds['vc3']
+                    self.log.debug("vc3 section already in doc. Doing nothing.")
+                except KeyError:
+                    # no vc3 section
+                    self.log.debug("No vc3 section in doc.")
+                    ccstr = self.parent.parent.ssca.getcertchain()
+                    eccstr = self.parent.parent.infoclient.encode(ccstr)
+                    ds['vc3'] = { "ca-chain" : eccstr, 
+                                  "encoding" : "base64" }
+                    jd = json.dumps(ds)
+                    self.parent.parent.infoclient.storedocument('vc3',jd)
+            except Exception as e:
+                self.log.error("Exception: %s" % e)
         
             
