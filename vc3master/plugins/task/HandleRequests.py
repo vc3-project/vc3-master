129cc40e22e8b53623230d702118d5186a907341
Kenyi Hurtado <khurtado@nd.edu>
2018-05-04 15:47:42 -0400

    Add a workaround to batch_gahp/ transform ads bug dealing with multicore jobs through blah

--- vc3master/plugins/task/HandleRequests.py
+++ vc3master/plugins/task/HandleRequests.py
@@ -377,6 +377,8 @@ class HandleRequests(VC3Task):
             config.set(section_name, 'batchsubmit.condorlocal.condor_attributes.request_cpus',   cores)
             config.set(section_name, 'batchsubmit.condorlocal.condor_attributes.request_disk',   disk   * 1024)
             config.set(section_name, 'batchsubmit.condorlocal.condor_attributes.request_memory', memory * 1024)
+            # Workaround to (apparently) a bosco bug dealing with multicore
+            config.set(section_name, 'batchsubmit.condorlocal.condor_attributes.+request_nodenumber',   cores)
 
             config.set(section_name, 'executable',                 '%(builder)s')
         else:
