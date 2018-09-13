4257086b19620338503bd15ca105fafb33d9dbd5
Benjamin Tovar <btovar@nd.edu>
2018-09-13 13:27:51 -0400

    set app_peaceful, app_killorder accordingly

--- vc3master/plugins/task/HandleRequests.py
+++ vc3master/plugins/task/HandleRequests.py
@@ -364,6 +364,15 @@ class HandleRequests(VC3Task):
 
             config.set(section_name, 'executable',                  '%(builder)s')
 
+            if nodeset.app_peaceful is not None:
+                if nodeset.app_peaceful:
+                    config.set(section_name, 'batchsubmit.condorsshremotemanager.overlay.peaceful', 'yes')
+                else:
+                    config.set(section_name, 'batchsubmit.condorsshremotemanager.overlay.peaceful', 'no')
+
+            if nodeset.app_killorder is not None:
+                    config.set(section_name, 'batchsubmit.condorsshremotemanager.overlay.killorder', nodeset.app_killorder)
+
             if nodeset.app_type == 'htcondor' and nodeset.app_role == 'worker-nodes':
                 try:
                     headnode = self.client.getNodeset(request.headnode)
